dbf9ae950f15c300fcf3ec43fb88f86433ac84c1
Christos Korgialas <72823989+ckorgial@users.noreply.github.com>
2022-04-02 17:55:05 +0300

    Add files via upload

---
+++ mainCAAEsplit.py
@@ -0,0 +1,167 @@
+import modelsplit
+import consts
+import logging
+import os
+import re
+import numpy as np
+import argparse
+import sys
+import random
+import datetime
+import torch
+from utils import *
+from torchvision.datasets.folder import pil_loader
+import gc
+import torch
+
+gc.collect()
+
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+assert sys.version_info >= (3, 6),\
+    "This script requires Python >= 3.6"  # TODO 3.7?
+assert tuple(int(ver_num) for ver_num in torch.__version__.split('.')) >= (0, 4, 0),\
+    "This script requires PyTorch >= 0.4.0"  # TODO 0.4.1?
+
+
+def str_to_gender(s):
+    s = str(s).lower()
+    if s in ('m', 'man', '0'):
+        return 0
+    elif s in ('f', 'female', '1'):
+        return 1
+    else:
+        raise KeyError("No gender found")
+
+
+def str_to_bool(s):
+    s = s.lower()
+    if s in ('true', 't', 'yes', 'y', '1'):
+        return True
+    elif s in ('false', 'f', 'no', 'n', 'o'):
+        return False
+    else:
+        raise KeyError("Invalid boolean")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='AgeProgression on PyTorch.', formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--mode', choices=['train', 'test'], default='train')
+
+    # train params
+    parser.add_argument('--epochs', '-e', default=1, type=int)
+    parser.add_argument(
+        '--models-saving',
+        '--ms',
+        dest='models_saving',
+        choices=('always', 'last', 'tail', 'never'),
+        default='always',
+        type=str,
+        help='Model saving preference.{br}'
+             '\talways: Save trained model at the end of every epoch (default){br}'
+             '\tUse this option if you have a lot of free memory and you wish to experiment with the progress of your results.{br}'
+             '\tlast: Save trained model only at the end of the last epoch{br}'
+             '\tUse this option if you don\'t have a lot of free memory and removing large binary files is a costly operation.{br}'
+             '\ttail: "Safe-last". Save trained model at the end of every epoch and remove the saved model of the previous epoch{br}'
+             '\tUse this option if you don\'t have a lot of free memory and removing large binary files is a cheap operation.{br}'
+             '\tnever: Don\'t save trained model{br}'
+             '\tUse this option if you only wish to collect statistics and validation results.{br}'
+             'All options except \'never\' will also save when interrupted by the user.'.format(br=os.linesep)
+    )
+    parser.add_argument('--batch-size', '--bs', dest='batch_size', default=64, type=int)
+    parser.add_argument('--weight-decay', '--wd', dest='weight_decay', default=1e-5, type=float)
+    parser.add_argument('--learning-rate', '--lr', dest='learning_rate', default=2e-4, type=float)
+    parser.add_argument('--b1', '-b', dest='b1', default=0.9, type=float)
+    parser.add_argument('--b2', '-B', dest='b2', default=0.999, type=float)
+    parser.add_argument('--shouldplot', '--sp', dest='sp', default=False, type=bool)
+
+    # test params
+    parser.add_argument('--age', '-a', required=False, type=int)
+    parser.add_argument('--gender', '-g', required=False, type=str_to_gender)
+    parser.add_argument('--watermark', '-w', action='store_true')
+
+    # shared params
+    parser.add_argument('--cpu', '-c', action='store_true', help='Run on CPU even if CUDA is available.')
+    parser.add_argument('--load', '-l', required=False, default=None, help='Trained models path for pre-training or for testing')
+    parser.add_argument('--input', '-i', default=None, help='Training dataset path (default is {}) or testing image path'.format(default_train_results_dir()))
+    parser.add_argument('--output', '-o', default='')
+    parser.add_argument('-z', dest='z_channels', default=100, type=int, help='Length of Z vector')
+    args = parser.parse_args()
+
+    consts.NUM_Z_CHANNELS = args.z_channels
+    net = modelsplit.Net()
+
+    if not args.cpu and torch.cuda.is_available():
+        net.cuda()
+
+    if args.mode == 'train':
+
+        betas = (args.b1, args.b2) if args.load is None else None
+        weight_decay = args.weight_decay if args.load is None else None
+        lr = args.learning_rate if args.load is None else None
+
+        if args.load is not None:
+            net.load(args.load)
+            print("Loading pre-trained models from {}".format(args.load))
+
+        data_src = args.input or consts.UTKFACE_DEFAULT_PATH
+        print("Data folder is {}".format(data_src))
+        results_dest = args.output or default_train_results_dir()
+        os.makedirs(results_dest, exist_ok=True)
+        print("Results folder is {}".format(results_dest))
+
+        with open(os.path.join(results_dest, 'session_arguments.txt'), 'w') as info_file:
+            info_file.write(' '.join(sys.argv))
+
+        log_path = os.path.join(results_dest, 'log_results.log')
+        if os.path.exists(log_path):
+            os.remove(log_path)
+        logging.basicConfig(filename=log_path, level=logging.DEBUG)
+
+        net.teachSplit(
+            utkface_path=data_src,
+            batch_size=args.batch_size,
+            betas=betas,
+            epochs=args.epochs,
+            weight_decay=weight_decay,
+            lr=lr,
+            should_plot=args.sp,
+            where_to_save=results_dest,
+            models_saving=args.models_saving,
+            explainable=False, # Change to True for enabling the xAI-System - Keep False for the Original CAAE
+            explanation_type='saliency'# If explainable = True, choose the explanatory method by typing saliency or shap or lime
+        )
+
+    elif args.mode == 'test':
+
+        if args.load is None:
+            raise RuntimeError("Must provide path of trained models")
+
+        net.load(path=args.load, slim=True)
+
+        results_dest = args.output or default_test_results_dir()
+        results_dest = os.path.join(results_dest, str(args.age) + '.' + str(args.gender))
+        if not os.path.isdir(results_dest):
+            os.makedirs(results_dest)
+
+        args.input = os.path.join(args.input, str(args.age) + '.' + str(args.gender))
+
+        for x in range(0, consts.NUM_AGES):
+            if not os.path.exists(os.path.join(results_dest, str(args.age) + '.' + str(args.gender) + '_to_' + str(x) + '.' + str(args.gender))):
+                os.makedirs(os.path.join(results_dest, str(args.age) + '.' + str(args.gender) + '_to_' + str(x) + '.' + str(args.gender)))
+        if not os.path.exists(os.path.join(results_dest, str(args.age) + '.' + str(args.gender) + '_to_all')):
+            os.makedirs(os.path.join(results_dest,str(args.age) + '.' + str(args.gender) + '_to_all'))
+
+        images = os.listdir(args.input)
+        for image_name in images:
+            image_tensor = pil_to_model_tensor_transform(pil_loader(os.path.join(args.input, image_name))).to(net.device)
+            net.my_test_single(
+                image_tensor=image_tensor,
+                image_name=image_name,
+                age=args.age,
+                gender=args.gender,
+                target=results_dest,
+                watermark=args.watermark
+            )
---
+++ utils.py
@@ -0,0 +1,295 @@
+import os
+import datetime
+from shutil import copyfile
+from collections import namedtuple, defaultdict
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import mean_squared_error as mse
+
+import torch
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
+
+import consts
+import matplotlib
+matplotlib.use('Agg')
+
+def save_image_normalized(*args, **kwargs):
+    save_image(*args, **kwargs, normalize=True, range=(-1, 1), padding=4)
+
+
+def two_sided(x):
+    return 2 * (x - 0.5)
+
+def one_sided(x):
+    return (x + 1) / 2
+
+
+pil_to_model_tensor_transform = transforms.Compose(
+    [
+        transforms.Resize(size=(128, 128)),
+        transforms.ToTensor(),
+        transforms.Lambda(lambda t: t.mul(2).sub(1))  # Tensor elements domain: [0:1] -> [-1:1]
+    ]
+)
+
+
+def get_utkface_dataset(root):
+    print(root)
+    ret = lambda: ImageFolder(os.path.join(root, 'labeled'), transform=pil_to_model_tensor_transform)
+    try:
+        return ret()
+    except (RuntimeError, FileNotFoundError):
+        sort_to_classes(os.path.join(root, 'unlabeled'), print_cycle=1000)
+        return ret()
+
+def sort_to_classes(root, print_cycle=np.inf):
+    # Example UTKFace cropped and aligned image file format: [age]_[gender]_[race]_[date&time].jpg.chip.jpg
+    # Should be 23613 images, use print_cycle >= 1000
+    # Make sure you have > 100 MB free space
+
+    def log(text):
+        print('[UTKFace dset labeler] ' + text)
+
+    log('Starting labeling process...')
+    files = [f for f in os.listdir(root) if os.path.isfile(os.path.join(root, f))]
+    if not files:
+        raise FileNotFoundError('No image files in '+root)
+    copied_count = 0
+    sorted_folder = os.path.join(root, '..', 'labeled')
+    if not os.path.isdir(sorted_folder):
+        os.mkdir(sorted_folder)
+
+    for f in files:
+        matcher = consts.UTKFACE_ORIGINAL_IMAGE_FORMAT.match(f)
+        if matcher is None:
+            continue
+        age, gender, dtime = matcher.groups()
+        srcfile = os.path.join(root, f)
+        label = Label(int(age), int(gender))
+        dstfolder = os.path.join(sorted_folder, label.to_str())
+        dstfile = os.path.join(dstfolder, dtime+'.jpg')
+        if os.path.isfile(dstfile):
+            continue
+        if not os.path.isdir(dstfolder):
+            os.mkdir(dstfolder)
+        copyfile(srcfile, dstfile)
+        copied_count += 1
+        if copied_count % print_cycle == 0:
+            log('Copied %d files.' % copied_count)
+    log('Finished labeling process.')
+
+
+def get_fgnet_person_loader(root):
+    return DataLoader(dataset=ImageFolder(root, transform=pil_to_model_tensor_transform), batch_size=1)
+
+
+def str_to_tensor(text, normalize=False):
+    age_group, gender = text.split('.')
+    age_tensor = -torch.ones(consts.NUM_AGES)
+    age_tensor[int(age_group)] *= -1
+    gender_tensor = -torch.ones(consts.NUM_GENDERS)
+    gender_tensor[int(gender)] *= -1
+    if normalize:
+        gender_tensor = gender_tensor.repeat(consts.NUM_AGES // consts.NUM_GENDERS)
+    result = torch.cat((age_tensor, gender_tensor), 0)
+    return result
+
+
+class Label(namedtuple('Label', ('age', 'gender'))):
+    def __init__(self, age, gender):
+        super(Label, self).__init__()
+        self.age_group = self.age_transform(self.age)
+
+    def to_str(self):
+        return '%d.%d' % (self.age_group, self.gender)
+
+    @staticmethod
+    def age_transform(age):
+        age -= 1
+        if age < 20:
+            # first 4 age groups are for kids <= 20, 5 years intervals
+            return max(age // 5, 0)
+        else:
+            # last (6?) age groups are for adults > 20, 10 years intervals
+            return min(4 + (age - 20) // 10, consts.NUM_AGES - 1)
+
+    def to_tensor(self, normalize=False):
+        return str_to_tensor(self.to_str(), normalize=normalize)
+
+
+
+fmt_t = "%H_%M"
+fmt = "%Y_%m_%d"
+
+def default_train_results_dir():
+    return os.path.join('.', 'trained_models', datetime.datetime.now().strftime(fmt), datetime.datetime.now().strftime(fmt_t))
+
+
+def default_where_to_save(eval=True):
+    path_str = os.path.join('.', 'results', datetime.datetime.now().strftime(fmt), datetime.datetime.now().strftime(fmt_t))
+    if not os.path.exists(path_str):
+        os.makedirs(path_str)
+
+
+def default_test_results_dir(eval=True):
+    return os.path.join('.', 'test_results', datetime.datetime.now().strftime(fmt) if eval else fmt)
+
+
+def print_timestamp(s):
+    print("[{}] {}".format(datetime.datetime.now().strftime(fmt_t.replace('_', ':')), s))
+
+
+class LossTracker(object):
+    def __init__(self, use_heuristics=False, plot=False, eps=1e-3):
+        # assert 'train' in names and 'valid' in names, str(names)
+        self.losses = defaultdict(lambda: [])
+        self.paths = []
+        self.epochs = 0
+        self.use_heuristics = use_heuristics
+        if plot:
+           # print("names[-1] - "+names[-1])
+            plt.ion()
+            plt.show()
+        else:
+            plt.switch_backend("agg")
+
+    # deprecated
+    def append(self, train_loss, valid_loss, tv_loss, uni_loss, path):
+        self.train_losses.append(train_loss)
+        self.valid_losses.append(valid_loss)
+        self.tv_losses.append(tv_loss)
+        self.uni_losses.append(uni_loss)
+        self.paths.append(path)
+        self.epochs += 1
+        if self.use_heuristics and self.epochs >= 2:
+            delta_train = self.train_losses[-1] - self.train_losses[-2]
+            delta_valid = self.valid_losses[-1] - self.valid_losses[-2]
+            if delta_train < -self.eps and delta_valid < -self.eps:
+                pass  # good fit, continue training
+            elif delta_train < -self.eps and delta_valid > +self.eps:
+                pass  # overfit, consider stop the training now
+            elif delta_train > +self.eps and delta_valid > +self.eps:
+                pass  # underfit, if this is in an advanced epoch, break
+            elif delta_train > +self.eps and delta_valid < -self.eps:
+                pass  # unknown fit, check your model, optimizers and loss functions
+            elif 0 < delta_train < +self.eps and self.epochs >= 3:
+                prev_delta_train = self.train_losses[-2] - self.train_losses[-3]
+                if 0 < prev_delta_train < +self.eps:
+                    pass  # our training loss is increasing but in less than eps,
+                    # this is a drift that needs to be caught, consider lower eps next time
+            else:
+                pass  # saturation \ small fluctuations
+
+    def append_single(self, name, value):
+        self.losses[name].append(value)
+
+    def append_many(self, **names):
+        for name, value in names.items():
+            self.append_single(name, value)
+
+    def append_many_and_plot(self, **names):
+        self.append_many(**names)
+
+    def plot(self):
+        print("in plot")
+        plt.clf()
+        graphs = [plt.plot(loss, label=name)[0] for name, loss in self.losses.items()]
+        plt.legend(handles=graphs)
+        plt.xlabel('Epochs')
+        plt.ylabel('Averaged loss')
+        plt.title('Losses by epoch')
+        plt.grid(True)
+        plt.draw()
+        plt.pause(0.001)
+
+    @staticmethod
+    def show():
+        print("in show")
+        plt.show()
+
+    @staticmethod
+    def save(path):
+        plt.savefig(path, transparent=True)
+
+    def __repr__(self):
+        ret = {}
+        for name, value in self.losses.items():
+            ret[name] = value[-1]
+        return str(ret)
+
+
+def mean(l):
+    return np.array(l).mean()
+
+
+def uni_loss(input):
+    assert len(input.shape) == 2
+    batch_size, input_size = input.size()
+    hist = torch.histc(input=input, bins=input_size, min=-1, max=1)
+    return mse(hist, batch_size * torch.ones_like(hist)) / input_size
+
+
+def easy_deconv(in_dims, out_dims, kernel, stride=1, groups=1, bias=True, dilation=1):
+    if isinstance(kernel, int):
+        kernel = (kernel, kernel)
+    if isinstance(stride, int):
+        stride = (stride, stride)
+
+    c_in, h_in, w_in = in_dims
+    c_out, h_out, w_out = out_dims
+
+    padding = [0, 0]
+    output_padding = [0, 0]
+
+    lhs_0 = -h_out + (h_in - 1) * stride[0] + kernel[0]  # = 2p[0] - o[0]
+    if lhs_0 % 2 == 0:
+        padding[0] = lhs_0 // 2
+    else:
+        padding[0] = lhs_0 // 2 + 1
+        output_padding[0] = 1
+
+    lhs_1 = -w_out + (w_in - 1) * stride[1] + kernel[1]  # = 2p[1] - o[1]
+    if lhs_1 % 2 == 0:
+        padding[1] = lhs_1 // 2
+    else:
+        padding[1] = lhs_1 // 2 + 1
+        output_padding[1] = 1
+
+    return torch.nn.ConvTranspose2d(
+        in_channels=c_in,
+        out_channels=c_out,
+        kernel_size=kernel,
+        stride=stride,
+        padding=tuple(padding),
+        output_padding=tuple(output_padding),
+        groups=groups,
+        bias=bias,
+        dilation=dilation
+    )
+
+
+def remove_trained(folder):
+    if os.path.isdir(folder):
+        removed_ctr = 0
+        for tm in os.listdir(folder):
+            tm = os.path.join(folder, tm)
+            if os.path.splitext(tm)[1] == consts.TRAINED_MODEL_EXT:
+                try:
+                    os.remove(tm)
+                    removed_ctr += 1
+                except OSError as e:
+                    print("Failed removing {}: {}".format(tm, e))
+        if removed_ctr > 0:
+            print("Removed {} trained models from {}".format(removed_ctr, folder))
+
+
+def merge_images(batch1, batch2):
+    assert batch1.shape == batch2.shape
+    merged = torch.zeros(batch1.size(0) * 2, batch1.size(1), batch1.size(2), batch1.size(3), dtype=batch1.dtype)
+    for i, (image1, image2) in enumerate(zip(batch1, batch2)):
+        merged[2 * i] = image1
+        merged[2 * i + 1] = image2
+    return merged
