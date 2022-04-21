423748289c8b2bb29d3fdb885dacaeb4d3b3060f
Christos Korgialas <72823989+ckorgial@users.noreply.github.com>
2022-04-21 23:39:26 +0300

    Update mainCAAEsplit.py

--- mainCAAEsplit.py
+++ mainCAAEsplit.py
@@ -130,8 +130,8 @@ if __name__ == '__main__':
             should_plot=args.sp,
             where_to_save=results_dest,
             models_saving=args.models_saving,
-            explainable=False, # Change to True for enabling the xAI-System - Keep False for the Original CAAE
-            explanation_type='saliency'# If explainable = True, choose the explanatory method by typing saliency or shap or lime
+            explainable=False, # Change to True for enabling the xAI-System - Keep False for the Original CAAE !
+            explanation_type='saliency'# If explainable = True, choose the explanatory method by typing 'saliency' or 'shap' !
         )
 
     elif args.mode == 'test':
